b40fa987ec20d355c752716eb08e5c6d15371235
the_sky_of_tears <theskyoftears13@gmail.com>
2022-09-28 17:19:12 +0300

    [Fix] Naming convention

--- src/com/da/kpi/car/Car.java
+++ src/com/da/kpi/car/Car.java
@@ -22,13 +22,13 @@ public class Car {
 	public void printFullInfo() {
 		System.out.println("===================================");
 
-		System.out.println(/*"ID: " + this.id +
-				"\n*/"Brand: " + this.brand +
-				"\nModel: " + this.model +
-				"\nYear: " + this.year +
-				"\nColor: " + this.color +
-				"\nPrice: " + this.price +
-				"\nRegistration Number: " + this.regNumber);
+		System.out.println(/*"ID: " + id +
+				"\n*/"Brand: " + brand +
+				"\nModel: " + model +
+				"\nYear: " + year +
+				"\nColor: " + color +
+				"\nPrice: " + price +
+				"\nRegistration Number: " + regNumber);
 
 		System.out.println("===================================");
 	}
@@ -37,27 +37,27 @@ public class Car {
 
 		/*int idMin = 1000,
 				idMax = 100000;
-		this.id = ThreadLocalRandom.current().nextInt(idMin,idMax);*/
+		id = ThreadLocalRandom.current().nextInt(idMin,idMax);*/
 
-		this.brand = Brand.values()[ThreadLocalRandom.current().
+		brand = Brand.values()[ThreadLocalRandom.current().
 				nextInt(0, Brand.values().length)];
 
-		this.model = Model.values()[ThreadLocalRandom.current().
+		model = Model.values()[ThreadLocalRandom.current().
 				nextInt(0, Model.values().length)];
 
 		int yearMin = 1980,
 				yearMax = 2023; //2023 is not included
-		this.year = ThreadLocalRandom.current().nextInt(yearMin, yearMax);
+		year = ThreadLocalRandom.current().nextInt(yearMin, yearMax);
 
-		this.color = Color.values()[ThreadLocalRandom.current().
+		color = Color.values()[ThreadLocalRandom.current().
 				nextInt(0, Color.values().length)];
 
 		float priceMin = 10000,
 				priceMax = 1000000;
-		this.price = ThreadLocalRandom.current().nextFloat(priceMin, priceMax);
+		price = ThreadLocalRandom.current().nextFloat(priceMin, priceMax);
 
 		int regNumMin = 1000,
 				regNumMax = 10000; //10000 is not included
-		this.regNumber = ThreadLocalRandom.current().nextInt(regNumMin,regNumMax);
+		regNumber = ThreadLocalRandom.current().nextInt(regNumMin,regNumMax);
 	}
 }
