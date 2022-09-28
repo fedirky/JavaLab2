813d7ac44870146433d91f1f933cfb15f27bee45
the_sky_of_tears <theskyoftears13@gmail.com>
2022-09-28 08:35:29 +0300

    Initial

---
+++ src/com/da/kpi/Car.java
@@ -0,0 +1,23 @@
+package com.da.kpi;
+
+public class Car {
+	private int id;
+	private String brand;
+	private String model;
+	private int year;
+	private String color;
+	private float price;
+	private int regNumber;
+
+	public int getId() { return id; }
+	public String getMark() { return brand; }
+	public String getModel() { return model; }
+	public int getYear() { return year; }
+	public String getColor() { return color; }
+	public float getPrice() { return price; }
+	public int getRegNumber() { return regNumber; }
+
+	public Car() {
+
+	}
+}
---
+++ src/com/da/kpi/Main.java
@@ -0,0 +1,7 @@
+package com.da.kpi;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("You're Welcome!");
+	}
+}
